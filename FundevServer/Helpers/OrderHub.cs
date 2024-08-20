41f0148c2759e010cce2a12798edd97519f7e4e5
PVP <phuphuc2k1@gmail.com>
2024-08-20 18:19:53 +0700

    add signalR

---
+++ FundevServer/Helpers/OrderHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FundevServer.Helpers
+{
+    public class OrderHub : Hub
+    {
+        public async Task NotifyNewOrder(string userId)
+        {
+            await Clients.User(userId).SendAsync("RefreshOrders");
+        }
+    }
+}
